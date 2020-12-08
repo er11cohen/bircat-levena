708ceaf28181de1063783636d666920ac2323aaf
eran_cohen <eran_cohen@malam-payroll.com>
2020-12-08 11:37:17 +0200

    Add Battery Optimization

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -21,6 +21,7 @@ export class HomePage implements OnInit {
     public settings: Settings;
     public nusach: typeof Nusach = Nusach;
     public isBlessed = false;
+    public showBatteryOptimizations = false;
 
     constructor(private settingsService: SettingsService,
                 public alertController: AlertController,
@@ -45,6 +46,8 @@ export class HomePage implements OnInit {
             launchDetails.action = null;
             this.openNextMonthModal();
         }
+
+        this.checkBatteryOptimizations();
     }
 
     public shareApp(): void {
@@ -73,6 +76,37 @@ export class HomePage implements OnInit {
         this.notificationsService.createBLNotifications(true);
         this.openNextMonthModal();
         this.isBlessed = true;
+    }
+
+    public async requestBatteryOptimizations(): Promise<void> {
+        const alert = await this.alertController.create({
+            header: this.translate.instant(this.dict.REMINDER_NOT_SHOWING).toString(),
+            message: this.translate.instant(this.dict.BATTERY_OPTIMIZATIONS_EXPLAIN).toString(),
+            buttons: [
+                {
+                    text:  this.translate.instant(this.dict.HAPPILY).toString(),
+                    handler: () => {
+                        // @ts-ignore
+                        cordova.plugins.DozeOptimize.RequestOptimizations();
+                    }
+                }
+            ]
+        });
+
+        await alert.present();
+    }
+
+    private checkBatteryOptimizations(): void {
+        if (!this.platformsService.isAndroid()) {
+            return;
+        }
+
+        // @ts-ignore
+        window.cordova.plugins.DozeOptimize.IsIgnoringBatteryOptimizations((response) => {
+            if (response === 'false') {
+                this.showBatteryOptimizations = true;
+            }
+        }, () => {});
     }
 
     private async openNextMonthModal(): Promise<void> {
